a298ab0ab48fdf6c0a32adfb7e154e5e4c23cc93
Vishal Dubey <hdubey64@gmail.com>
2024-12-01 17:54:33 +0530

    Animation

--- 001_threeJS/002_vite_setup/threeCode/src/main.js
+++ 001_threeJS/002_vite_setup/threeCode/src/main.js
@@ -35,6 +35,7 @@
 //NOTE: Rotation and Grouping in Three.js
 
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -44,49 +45,55 @@ const camera = new THREE.PerspectiveCamera(
    100
 );
 
+// camera.position.y = 5;
 camera.position.z = 5;
 
-const cubegeo = new THREE.BoxGeometry(1.5, 1.5, 1.5);
-const cubemat = new THREE.MeshBasicMaterial({
-   color: "red",
-   //  wireframe: true,
-});
-const cube = new THREE.Mesh(cubegeo, cubemat);
-// cube.position.x = -1;
+// const cubegeo = new THREE.TorusKnotGeometry(0.5, 0.1, 5, 5);
+// const cubemat = new THREE.MeshBasicMaterial({
+//    color: "green",
+//    wireframe: true,
+// });
+// const cube = new THREE.Mesh(cubegeo, cubemat);
 
-const spheregeo = new THREE.SphereGeometry(1, 10, 10);
-const spheremat = new THREE.MeshBasicMaterial({
+const geometry = new THREE.BufferGeometry();
+
+// create a simple square shape. We duplicate the top left and bottom right
+// vertices because each vertex needs to appear once per triangle.
+const vertices = new Float32Array(3000);
+
+for (let i = 0; i <= 1000 * 3; i++) {
+   vertices[i] = (Math.random() - 0.5) * 7;
+}
+
+geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
+const material = new THREE.MeshBasicMaterial({
    color: "green",
-   //  wireframe: true,
+   wireframe: true,
 });
-const sphere = new THREE.Mesh(spheregeo, spheremat);
+const mesh = new THREE.Mesh(geometry, material);
 
-// sphere.position.x = 1;
-
-const group = new THREE.Group();
-group.add(cube);
-group.add(sphere);
-
-// group.position.x = 0;
-
-scene.add(group);
+scene.add(mesh);
 
 const canvas = document.querySelector("canvas");
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+
 renderer.render(scene, camera);
+let clock = new THREE.Clock();
 function animate() {
    window.requestAnimationFrame(animate);
-   group.rotation.x += 0.01;
-   group.rotation.y += 0.03;
-   group.rotation.z += 0.03;
-   //  cube.rotation.x += 0.01;
-   //  cube.rotation.z += 0.03;
-   //  cube.rotation.y += 0.03;
-   //  group.position.x += 0.1;
-   //  group.position.y += 0.1;
-   //  group.position.z += 0.1;
 
+   // cube.rotation.x = clock.getElapsedTime() * 12;
+   // cube.rotation.y = clock.getElapsedTime() * 12;
+   // cube.rotation.z = clock.getElapsedTime();
+   // mesh.rotation.x += 0.01;
+   mesh.rotation.y += 0.01;
+   // mesh.rotation.z += 0.01;
+   controls.update();
+   camera.position.z += -0.001;
    renderer.render(scene, camera);
 }
 
